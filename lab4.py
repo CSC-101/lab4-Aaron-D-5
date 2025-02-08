462644a3672b298444dcfdba77e108832a94c8d9
Aaron-D-5 <adelga68@calpoly.edu>
2025-02-07 23:58:05 -0800

    First

--- lab4.py
+++ lab4.py
@@ -1,22 +1,48 @@
 import data
+import data
+import math
 
 # Write your functions for each part in the space below.
 
 # Part 1
+def first_element(l2:list[list[int]]) -> list[int]:
+    l1 = []
+    finalL = []
+    for x in range(len(l2)):
+        if len(l2[x]) > 0:
+            l1.append(l2[x])
+
+    for x in range(len(l1)):
+        finalL.append(l1[x][0])
+    return finalL
 
 
 # Part 2
-
-
+def x_coord(l1:list[data.Point]) -> list[float]:
+    l2 = []
+    for x in range(len(l1)):
+        if l1[x].x >= 0 and l1[x].y >= 0:
+            l2.append(l1[x])
 # Part 3
-
+def are_in_positive_quad(l1:list[data.Point]) -> list[data.Point]:
+    l2 = []
+    for x in range(len(l1)):
+        if l1[x].x >= 0 and l1[x].y >= 0:
+            l2.append(l1[x])
+    return l2
 
 # Part 4
-
+def distance(p1:data.Point, p2:data.Point) -> float:
+    return math.sqrt((p1.x - p2.x)**2 + (p1.y - p2.y)**2)
 
 # Part 5
-
+def manhattan_distance(p1:data.Point, p2:data.Point) -> float:
+    return abs(p1.x - p2.x) + abs(p1.y - p2.y)
 
 # Part 6
-
+def distance_all(l1:list[data.Point]) -> list[float]:
+    l2 = []
+    for x in range(len(l1)):
+        l2.append(math.sqrt(l1[x].x ** 2 + l1[x].y ** 2))
+    return l2
 
--- lab4_tests.py
+++ lab4_tests.py
@@ -1,8 +1,7 @@
 import data
 import lab4
 import unittest
-
-
+import math
 # Write your test cases for each part below.
 
 class TestCases(unittest.TestCase):
@@ -16,22 +15,84 @@ class TestCases(unittest.TestCase):
 
     def test_first_element_2(self):
         # write a second test here
-
+        input = [[3,5], [7,6],[],[2,1]]
+        result = lab4.first_element(input)
+        expected = [3,7,2]
+        self.assertEqual(expected, result)
 
     # Part 2
+    def test_second_element_1(self):
+        p1 = data.Point(1,2)
+        p2 = data.Point(2,3)
+        input = [p1, p2]
+        result = lab4.x_coord(input)
+        expected = [2,3]
+        self.assertEqual(expected, result)
 
+    def test_second_element_2(self):
+        p1 = data.Point(9,2)
+        p2 = data.Point(5,3)
+        input = [p1, p2]
+        result = lab4.x_coord(input)
+        expected = [9,5]
+        self.assertEqual(expected, result)
 
     # Part 3
+    def test_third_element_1(self):
+        p1 = data.Point(1,2)
+        p2 = data.Point(3,4)
+        input = [p1, p2]
+        result = lab4.are_in_positive_quad(input)
+        expected = [p1,p2]
+        self.assertEqual(expected, result)
 
-
+    def test_third_element_2(self):
+        p1 = data.Point(1,2)
+        p2 = data.Point(3,-4)
+        input = [p1, p2]
+        result = lab4.are_in_positive_quad(input)
+        expected = [p1]
+        self.assertEqual(expected, result)
     # Part 4
-
+    def test_fourth_element_1(self):
+        p1 = data.Point(1,2)
+        p2 = data.Point(3,4)
+        result = lab4.distance(p1,p2)
+        expected = 2 * math.sqrt(2)
+        self.assertEqual(expected, result, place=0)
 
     # Part 5
-
+    def test_five_element_1(self):
+        p1 = data.Point(1,2)
+        p2 = data.Point(3,4)
+        result = lab4.manhattan_distance([p1,p2])
+        expected = 1
+        self.assertEqual(expected, result)
+    def test_five_element_2(self):
+        p1 = data.Point(4,5)
+        p2 = data.Point(6,7)
+        result = lab4.manhattan_distance(p1,p2)
+        expected = 4
+        self.assertEqual(expected, result)
 
     # Part 6
+    def test_six_element_1(self):
+        p1 = data.Point(3,4)
+        p2 = data.Point(6,8)
+        p3 = data.Point(9,12)
+        p4 = data.Point(12,16)
+        input = [p1, p2, p3, p4]
+        result = lab4.distance_all(input)
+        expected = [5,10,15,20]
+        self.assertEqual(expected, result)
 
+    def test_six_element_2(self):
+        p1 = data.Point(24,18)
+        p2 = data.Point(32,24)
+        input = [p1, p2]
+        result = lab4.distance_all(input)
+        expected = [30,40]
+        self.assertEqual(expected, result)
 
 
 
